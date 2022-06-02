d4105887ebe2d97a6002a9b51750df44c806e3b6
Alena Katkova <katkova.web@gmail.com>
2022-06-02 17:23:50 +0200

    get ditto from poke api

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,35 @@
 import add from "./add";
+import axios from "axios";
 
 let a: number = 12;
 
-console.log(add(a, 12));
+console.log(add(a, 99));
+
+interface Ability {
+    ability: {
+        name: string;
+        url: string;
+    };
+}
+
+interface PokemonDetails {
+    abilities: Array<Ability>
+    name: string;
+    height: number;
+}
+
+const getPokemonDetails = async (pokemonName: string) : Promise<PokemonDetails>=> {
+    try {
+        const { data, status } = await axios.get("https://pokeapi.co/api/v2/pokemon/ditto");
+        console.log(JSON.stringify(data))
+        console.log('response status is: ', status);
+        return data;
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+const l = await getPokemonDetails("nice")
+
+console.log(l)
+

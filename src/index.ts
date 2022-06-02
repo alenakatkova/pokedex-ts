73c5e81dbb572448a772cec18b99e84418de8015
Alena Katkova <katkova.web@gmail.com>
2022-06-02 21:52:21 +0200

    show object with 10 pokemons on page

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,11 @@ import getPokemonDetails from "./api/getPokemonDetails";
 import getSetAmountOfPokemons from "./api/getSetAmountOfPokemons";
 import PokemonList from "./interfaces/Pokemons";
 
-const pokemon : PokemonDetails = await getPokemonDetails("pikachu")
-const pokemons : PokemonList = await getSetAmountOfPokemons(10, 0)
+const pokemon : PokemonDetails = await getPokemonDetails("pikachu");
+const pokemons : PokemonList = await getSetAmountOfPokemons(10, 0);
+
+const board = document.getElementById("board");
+board.innerText = JSON.stringify(pokemons)
 
 console.log(pokemons)
 

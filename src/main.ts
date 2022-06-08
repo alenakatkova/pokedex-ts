2eaf9b071d360ce3a80c0a86bfd7f992dc30f0ff
Alena Katkova <katkova.web@gmail.com>
2022-06-08 17:02:02 +0200

    move pagination related functions to pagination module

--- src/main.ts
+++ src/main.ts
@@ -1,39 +1,7 @@
 import getPokemons from "./api/getPokemons";
 import fillBoardWithPokemons from "./fillBoardWithPokemons";
-import {updateBtnsAvailability} from "./pagination";
+import {addPageBtnsListeners, removePageBtnsListeners, updateBtnsAvailability} from "./pagination";
 import {PokemonList} from "./interfaces/PokemonList";
-
-async function changePage(url: string) {
-    const container = document.getElementById("content-container");
-    const pokemonList = await getPokemons(url, 10);
-    window.NEXT_LIST_URL = pokemonList.next;
-    window.PREV_LIST_URL = pokemonList.previous;
-    container.innerHTML = "";
-    fillBoardWithPokemons(pokemonList.results);
-    updateBtnsAvailability(window.PREV_LIST_URL, window.NEXT_LIST_URL);
-}
-
-async function handleNextBtnClick() {
-    await changePage(window.NEXT_LIST_URL);
-}
-
-async function handlePrevBtnClick() {
-    await changePage(window.PREV_LIST_URL);
-}
-
-function addPageBtnsListeners() {
-    const prevBtn = document.getElementById("previous");
-    const nextBtn = document.getElementById("next");
-    nextBtn.addEventListener("click", handleNextBtnClick);
-    prevBtn.addEventListener("click", handlePrevBtnClick);
-}
-
-function removePageBtnsListeners() {
-    const prevBtn = document.getElementById("previous");
-    const nextBtn = document.getElementById("next");
-    nextBtn.removeEventListener("click", handleNextBtnClick);
-    prevBtn.removeEventListener("click", handlePrevBtnClick);
-}
 
 function retrieveInitialPokemonList(): Promise<PokemonList> {
     return getPokemons("https://pokeapi.co/api/v2/pokemon?offset=0&limit=10", 10);
--- src/pagination.ts
+++ src/pagination.ts
@@ -1,3 +1,6 @@
+import getPokemons from "./api/getPokemons";
+import fillBoardWithPokemons from "./fillBoardWithPokemons";
+
 function toggleBtnDisableAttribute(btn: HTMLElement, shouldBeDisabled: boolean) {
     if (shouldBeDisabled) {
         btn.setAttribute("disabled", "");
@@ -6,6 +9,7 @@ function toggleBtnDisableAttribute(btn: HTMLElement, shouldBeDisabled: boolean) 
     }
 }
 
+// TODO disable btns when mode is not chosen
 export function updateBtnsAvailability(previousUrl: string, nextUrl: string) {
     const prevBtn = document.getElementById("previous");
     const nextBtn = document.getElementById("next");
@@ -13,3 +17,34 @@ export function updateBtnsAvailability(previousUrl: string, nextUrl: string) {
     toggleBtnDisableAttribute(nextBtn, nextUrl === null);
 }
 
+async function changePage(url: string) {
+    const container = document.getElementById("content-container");
+    const pokemonList = await getPokemons(url, 10);
+    window.NEXT_LIST_URL = pokemonList.next;
+    window.PREV_LIST_URL = pokemonList.previous;
+    container.innerHTML = "";
+    fillBoardWithPokemons(pokemonList.results);
+    updateBtnsAvailability(window.PREV_LIST_URL, window.NEXT_LIST_URL);
+}
+
+async function handleNextBtnClick() {
+    await changePage(window.NEXT_LIST_URL);
+}
+
+async function handlePrevBtnClick() {
+    await changePage(window.PREV_LIST_URL);
+}
+
+export function addPageBtnsListeners() {
+    const prevBtn = document.getElementById("previous");
+    const nextBtn = document.getElementById("next");
+    nextBtn.addEventListener("click", handleNextBtnClick);
+    prevBtn.addEventListener("click", handlePrevBtnClick);
+}
+
+export function removePageBtnsListeners() {
+    const prevBtn = document.getElementById("previous");
+    const nextBtn = document.getElementById("next");
+    nextBtn.removeEventListener("click", handleNextBtnClick);
+    prevBtn.removeEventListener("click", handlePrevBtnClick);
+}
